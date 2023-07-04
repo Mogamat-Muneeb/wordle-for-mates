6c2104a7003eb15f9bac515a9a1d42323e6db1f9
Mogamat Muneeb Davids <davimuneeb785@gmail.com>
2023-07-04 20:45:58 +0200

    wip

--- src/components/CreateGame.js
+++ src/components/CreateGame.js
@@ -1,6 +1,5 @@
-
 // import React, { useState } from "react";
-// import { AES } from "crypto-js";
+// import sjcl from "sjcl";
 
 // const CreateGame = () => {
 //   const [word, setWord] = useState("");
@@ -16,28 +15,42 @@
 
 //   const generateLink = () => {
 //     const secretKey = "muneeb2905";
-//     const encryptedWord = AES.encrypt(word, secretKey).toString();
-//     const link = `http://localhost:3000/wordle?word=${encryptedWord}&name=${name}`;
+//     const encryptedWord = sjcl.encrypt(secretKey, word);
+//     const link = `http://localhost:3000/wordle?word=${encodeURIComponent(
+//       encryptedWord
+//     )}&name=${encodeURIComponent(name)}`;
 //     console.log(link, "generated link");
+
+//     navigator.clipboard
+//       .writeText(link)
+//       .then(() => {
+//         const createLinkButton = document.getElementById("createLinkButton");
+//         createLinkButton.textContent = "Link Copied";
+//       })
+//       .catch((error) => {
+//         console.error("Failed to copy the link to the clipboard:", error);
+//       });
 //   };
 
 //   return (
 //     <div>
 //       <h1>Create a Wordle game using your word!</h1>
-//       <form>
+//       <form className="w-10/12 max-w-md pb-24 mx-auto mt-16 space-y-8 text-center">
 //         <input
 //           type="text"
 //           placeholder="5 letter word"
 //           value={word}
 //           onChange={handleWordChange}
+//           className="w-full p-4 text-3xl font-bold text-center text-black rounded outline-none bg-slate-300 placeholder:text-white"
 //         />
 //         <input
 //           type="text"
 //           placeholder="name"
 //           value={name}
 //           onChange={handleNameChange}
+//           className="w-full p-4 text-3xl font-bold text-center text-black rounded outline-none bg-slate-300 placeholder:text-white"
 //         />
-//         <p  id="createLinkButton" onClick={generateLink}>
+//         <p id="createLinkButton" onClick={generateLink}>
 //           Create Link
 //         </p>
 //       </form>
@@ -47,13 +60,13 @@
 
 // export default CreateGame;
 
-
 import React, { useState } from "react";
 import sjcl from "sjcl";
 
 const CreateGame = () => {
   const [word, setWord] = useState("");
   const [name, setName] = useState("");
+  const [linkCopied, setLinkCopied] = useState(false);
 
   const handleWordChange = (event) => {
     setWord(event.target.value);
@@ -66,30 +79,59 @@ const CreateGame = () => {
   const generateLink = () => {
     const secretKey = "muneeb2905";
     const encryptedWord = sjcl.encrypt(secretKey, word);
-    const link = `http://localhost:3000/wordle?word=${encodeURIComponent(encryptedWord)}&name=${encodeURIComponent(name)}`;
+    const link = `http://localhost:3000/wordle?word=${encodeURIComponent(
+      encryptedWord
+    )}&name=${encodeURIComponent(name)}`;
     console.log(link, "generated link");
+
+    navigator.clipboard
+      .writeText(link)
+      .then(() => {
+        setLinkCopied(true);
+      })
+      .catch((error) => {
+        console.error("Failed to copy the link to the clipboard:", error);
+      });
   };
 
   return (
     <div>
-      <h1>Create a Wordle game using your word!</h1>
+      <h1 className="font-extrabold text-[30px]">Wordle for mates!</h1>
+      <p className="font-extrabold text-[20px] text-[#212529]">
+        Create a
+        <a
+          href="https://www.nytimes.com/games/wordle/index.html"
+          className="px-2 underline"
+        >
+          Wordle
+        </a>
+        game using your word!
+      </p>
+      <p className="font-extrabold text-[16px] text-[#212529]">Enter a 5 letter word to get started.</p>
       <form className="w-10/12 max-w-md pb-24 mx-auto mt-16 space-y-8 text-center">
         <input
           type="text"
           placeholder="5 letter word"
           value={word}
           onChange={handleWordChange}
-          className="w-full p-4 text-3xl font-bold text-center text-black rounded outline-none bg-slate-300 placeholder:text-white"
+          className="w-full p-4 text-xl font-bold text-center text-black rounded outline-none bg-[#e9ecef] placeholder:text-white"
         />
         <input
           type="text"
           placeholder="name"
           value={name}
           onChange={handleNameChange}
-          className="w-full p-4 text-3xl font-bold text-center text-black rounded outline-none bg-slate-300 placeholder:text-white"
+          className="w-full p-4 text-xl font-bold text-center text-black rounded outline-none bg-[#e9ecef] placeholder:text-white"
         />
-        <p id="createLinkButton" onClick={generateLink}>
-          Create Link
+        <p
+          onClick={generateLink}
+          className={
+            linkCopied
+              ? "w-full p-4 text-lg font-bold text-center text-white rounded outline-none bg-[#20bf55] "
+              : "w-full p-4 text-lg font-bold text-center text-white rounded outline-none bg-[#212529]"
+          }
+        >
+          {linkCopied ? "Link Copied" : "Create Link"}
         </p>
       </form>
     </div>
@@ -97,4 +139,3 @@ const CreateGame = () => {
 };
 
 export default CreateGame;
-
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,33 +1,3 @@
-// import React, { useEffect, useState } from "react";
-// import { useLocation } from "react-router-dom";
-// import { AES, enc } from "crypto-js";
-// import Wordle from "./Wordle";
-
-// const Home = () => {
-//   const location = useLocation();
-//   const params = new URLSearchParams(location.search);
-//   console.log(params, "params")
-//   const encryptedWord = params.get("word");
-//   const [solution, setSolution] = useState("");
-
-//   useEffect(() => {
-//     const secretKey = "muneeb2905";
-//     const decryptedWord = AES.decrypt(encryptedWord, secretKey).toString(
-//       enc.Utf8
-//     );
-//     setSolution(decryptedWord);
-//   }, [encryptedWord]);
-
-//   return (
-//     <div className="flex flex-col items-center justify-center">
-//       <h1>Wordle (Lingo)</h1>
-//       {solution && <Wordle solution={solution} guess={encryptedWord} />}
-//     </div>
-//   );
-// };
-
-// export default Home;
-
 import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import sjcl from "sjcl";
