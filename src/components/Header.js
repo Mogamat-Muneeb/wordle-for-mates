7ca7435a8a4e7b467c288ae8706700528a317975
Mogamat-Muneeb <davimuneeb785@gmail.com>
2023-07-06 12:07:48 +0200

    wip

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ function Header() {
   return (
     <div className="z-[1000]">
       <h1 className="z-[1000] font-extrabold text-[30px] p-1 bg-white text-[#212529] border-b-[1px] border-[#eee] w-full fixed right-0 left-0">
-        Wordle for mates!😀😀
+        Wordle for mates!
       </h1>
     </div>
   );
