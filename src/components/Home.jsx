920f0bdc4b43f5d066411ac99cb2228718b7acd6
Mogamat Muneeb Davids <davimuneeb785@gmail.com>
2023-09-22 16:30:10 +0200

    wip : added some styles

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,19 +4,22 @@ import { Link } from "react-router-dom";
 const Home = () => {
   return (
     <div className="flex flex-col items-center justify-between w-10/12 h-full max-w-md  mx-auto text-center pt-[20%]">
-      <p className=" p-4 md:text-2xl text-[20px] font-bold text-center text-[#212529]">
+      <p className=" px-4 pt-4 pb-2  md:text-xl text-[16px] font-extrabold text-center text-[#212529]">
         Get 6 chances to guess <br /> a 5-letter word.
+      </p>
+      <p className="pb-4 md:text-[16] text-[14px] font-normal text-center text-[#212529]">
+        Like Wordle but with a twist.
       </p>
       <div className="flex flex-col w-full gap-4 md:flex-row">
         <Link
           to="/wordle-single"
-          className="w-full p-4 text-lg font-bold text-center text-white rounded outline-none hover:bg-[#5ac85a] bg-[#212529] cursor-pointer"
+          className="w-full p-4 text-lg font-bold text-center text-white transition-all duration-30 rounded outline-none hover:bg-[#5ac85a] bg-[#212529] cursor-pointer"
         >
           Wordle
         </Link>
         <Link
           to="/with-a-friend"
-          className="w-full p-4 text-lg font-bold text-center text-white rounded outline-none hover:bg-[#5ac85a] bg-[#212529] cursor-pointer"
+          className="w-full p-4 text-lg font-bold text-center transition-all duration-30 text-white rounded outline-none hover:bg-[#5ac85a] bg-[#212529] cursor-pointer"
         >
           Wordle with a friend
         </Link>
