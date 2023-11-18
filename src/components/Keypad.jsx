03b05207d94c7462fa4a210926c1e37863fa839e
Mogamat Muneeb Davids <davimuneeb785@gmail.com>
2023-11-18 09:23:57 +0200

    wip

--- src/components/Keypad.jsx
+++ src/components/Keypad.jsx
@@ -66,7 +66,7 @@ export default function Keypad({ usedKeys, handleLetterClick }) {
                      ${
                        letter.key === "Enter" || letter.key === "Backspace"
                          ? "md:w-[100px] w-[50px] flex items-center justify-center "
-                         : "md:w-[30px] w-[25px] flex items-center justify-center "
+                         : "md:w-[40px] w-[25px] flex items-center justify-center "
                      }  cursor-pointer px-4 py-4  `}
                     onClick={() => handleClick(letter.key)}
                   >
--- src/components/WordleWithFriend.jsx
+++ src/components/WordleWithFriend.jsx
@@ -3,22 +3,33 @@ import { useLocation } from "react-router-dom";
 import sjcl from "sjcl";
 import Wordle from "./Wordle";
 import HowToPlay from "./HowToPlay";
+import { encrypt } from "../helper";
 
 const WordleWithFriend = () => {
   const location = useLocation();
   const params = new URLSearchParams(location.search);
   const encryptedWord = params.get("word");
   const createName = params.get("name");
-  const [solution, setSolution] = useState("");
+  // const [solution, setSolution] = useState("");
+  const [solutionEncrypt, setSolutionEncrypt] = useState("");
   const [show, setShow] = useState(false);
   const handleShow = () => {
     setShow(!show);
   };
+
   useEffect(() => {
     const secretKey = `${process.env.REACT_APP_SECRET_KEY}`;
     const decryptedWord = sjcl.decrypt(secretKey, encryptedWord);
-    setSolution(decryptedWord);
-  }, [encryptedWord]);
+    const shiftAmount = 3;
+    const encryptedText = encrypt(decryptedWord, shiftAmount);
+    setSolutionEncrypt(encryptedText);
+  }, [solutionEncrypt]);
+
+  // useEffect(() => {
+  //   const secretKey = `${process.env.REACT_APP_SECRET_KEY}`;
+  //   const decryptedWord = sjcl.decrypt(secretKey, encryptedWord);
+  //   setSolution(decryptedWord);
+  // }, [encryptedWord]);
 
   return (
     <div className="flex flex-col items-center justify-center">
@@ -29,11 +40,7 @@ const WordleWithFriend = () => {
       >
         How to play
       </button>
-      {/* <p className="py-4 font-medium">
-        You have 6 tries to guess
-        <span className="text-[#5ac85a] font-bold px-1" >{createName}'s</span> 5
-        letter word!
-      </p> */}
+
       <p className="py-1font-medium">
         You have 6 tries to guess
         <span className="text-[#5ac85a] font-bold px-1">
@@ -42,9 +49,9 @@ const WordleWithFriend = () => {
         5-letter word!
       </p>
 
-      {solution && (
+      {solutionEncrypt && (
         <Wordle
-          solution={solution}
+          solution={solutionEncrypt}
           guess={encryptedWord}
           createName={createName}
         />
